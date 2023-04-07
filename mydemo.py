58e79682c0e87c850569fd5735aafeaebfee70bd
Haim Vanunu <hvanunu@gmail.com>
2023-04-07 11:50:04 -0700

    dataset

---
+++ mydemo.py
@@ -0,0 +1,66 @@
+from video_dataset import  VideoFrameDataset, ImglistToTensor
+from torchvision import transforms
+import torch
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+import os
+
+def plot_video(rows, cols, frame_list, plot_width, plot_height, title: str):
+    fig = plt.figure(figsize=(plot_width, plot_height))
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                     nrows_ncols=(rows, cols),  # creates 2x2 grid of axes
+                     axes_pad=0.3,  # pad between axes in inch.
+                     )
+
+    for index, (ax, im) in enumerate(zip(grid, frame_list)):
+        # Iterating over the grid returns the Axes.
+        ax.imshow(im)
+        ax.set_title(index)
+    plt.suptitle(title)
+    plt.show()
+
+if __name__ == '__main__':
+    videos_root = os.path.join(os.getcwd(), 'demo_dataset')
+    annotation_file = os.path.join(videos_root, 'annotations.txt')
+
+    preprocess = transforms.Compose([
+        ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
+        transforms.Resize(299),  # image batch, resize smaller edge to 299
+        transforms.CenterCrop(299),  # image batch, center crop to square 299x299
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    dataset = VideoFrameDataset(
+        root_path=videos_root,
+        annotationfile_path=annotation_file,
+        num_segments=1,
+        frames_per_segment=9,
+        imagefile_template='img_{:05d}.jpg',
+        transform=preprocess,
+        test_mode=False
+    )
+
+    def denormalize(video_tensor):
+        inverse_normalize = transforms.Normalize(
+            mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.225],
+            std=[1 / 0.229, 1 / 0.224, 1 / 0.225]
+        )
+        return (inverse_normalize(video_tensor) * 255.).type(torch.uint8).permute(0, 2, 3, 1).numpy()
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset=dataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=1,
+        pin_memory=True
+    )
+
+    for epoch in range(10):
+        for video_batch, labels in dataloader:
+            print("\nVideo Batch Tensor Size:", video_batch.size())
+            print("Batch Labels Size:", labels.size())
+            break
+        break
+    print("Epoch:", epoch)
+
+
